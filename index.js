46f0a18e93ad4a536be5a628fd5e73653bd28eb0
Priscila Oliveira <poliveira13@hotmail.com>
2022-03-05 23:14:36 -0300

    app to src, settings in index

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /**
  * @format
  */
+import * as React from "react";
+import { AppRegistry } from "react-native";
+import App from "./src/App";
+import { name as appName } from "./app.json";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const Main = () => <App />;
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Main);
