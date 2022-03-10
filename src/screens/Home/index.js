26029c024ea2308e2e5f2fc8380654f0cc8067d1
Priscila Oliveira <poliveira13@hotmail.com>
2022-03-10 10:01:12 -0300

    get api data in home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, ScrollView } from 'react-native'
+
+import api from '../../services/api'
 
 import Bubbles from '../../components/Bubbles'
 
 import styles from './styles'
 
-const Home = async () => {
+const Home = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    api
+      .get()
+      .then((response) => {
+        const { data } = response.data
+
+        setData(data)
+      })
+      .catch((error) => console.log(error))
+  }, [setData])
+
   return (
     <>
       <View style={styles.container}>
